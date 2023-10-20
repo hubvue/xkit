9392d6a8a220e402517875d9cd9194d7fd1a29f4
cckcc.kim@gmail.com <kimwangchong@didiglobal.com>
2023-10-20 14:08:53 +0800

    test: add DoublyLinkedList test methods

--- structure/doublyLinkedlist_test.go
+++ structure/doublyLinkedlist_test.go
@@ -1,1 +1,63 @@
 package structure
+
+import "testing"
+
+func TestNewDoublyLinkedList(t *testing.T) {
+
+}
+
+func TestNewDoublyLinkedNode(t *testing.T) {
+
+}
+
+func TestDoublyLinkedList_Add(t *testing.T) {
+
+}
+
+func TestDoublyLinkedList_AddHead(t *testing.T) {
+
+}
+
+func TestDoublyLinkedList_Insert(t *testing.T) {
+
+}
+
+func TestDoublyLinkedList_Delete(t *testing.T) {
+
+}
+
+func TestDoublyLinkedList_DeleteHead(t *testing.T) {
+
+}
+
+func TestDoublyLinkedList_DeleteTail(t *testing.T) {
+
+}
+
+func TestDoublyLinkedList_Update(t *testing.T) {
+
+}
+
+func TestDoublyLinkedList_Has(t *testing.T) {
+
+}
+
+func TestDoublyLinkedList_Head(t *testing.T) {
+
+}
+
+func TestDoublyLinkedList_Tail(t *testing.T) {
+
+}
+
+func TestDoublyLinkedList_Size(t *testing.T) {
+
+}
+
+func TestDoublyLinkedList_Empty(t *testing.T) {
+
+}
+
+func TestDoublyLinkedList_Reverse(t *testing.T) {
+
+}
